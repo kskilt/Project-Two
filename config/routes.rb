332fffa59e8b6c36a0a7b80a2d7dfb9730662b7b
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-06-05 10:32:23 -0400

    added full routes to my controllers and set Welcome to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'movie/index'
-  get 'theater/index'
+  root 'welcome/index'
+  resources :movies
+  resources :theater
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
